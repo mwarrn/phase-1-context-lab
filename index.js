2273d18ba29ccf298f550a3c7cc4041d60bad6e7
Michael Warren <mwarrn@amazon.com>
2022-06-09 19:35:13 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-/* Your Code Here */
-
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
@@ -21,3 +19,62 @@ const allWagesFor = function () {
     return payable
 }
 
+const createEmployeeRecord = ([firstName, familyName, title, payPerHour]) => {
+    const employee = {
+      firstName: firstName,
+      familyName: familyName,
+      title: title,
+      payPerHour: payPerHour,
+      timeInEvents: [],
+      timeOutEvents: [],
+    }
+    return employee;
+}
+
+const createEmployeeRecords = (records) => {
+    return records.map(createEmployeeRecord);
+}
+
+function createTimeInEvent(timestamp) {
+    const dateArr = timestamp.split(' ');
+    const timeInObj = {
+      type: 'TimeIn',
+      hour: parseInt(dateArr[1].slice(0, 2) + '00'),
+      date: dateArr[0],
+    }
+    this.timeInEvents.push(timeInObj);
+    return this;
+}
+
+function createTimeOutEvent(timestamp) {
+    const dateArr = timestamp.split(' ');
+    const timeOutObj = {
+      type: 'TimeOut',
+      hour: parseInt(dateArr[1].slice(0, 2) + '00'),
+      date: dateArr[0],
+    }
+    this.timeOutEvents.push(timeOutObj);
+    return this;
+}
+
+function hoursWorkedOnDate(date) {
+    const timeIn = this.timeInEvents.find(day => date === day.date);
+    const timeOut = this.timeOutEvents.find(day => date === day.date);
+    return (timeOut.hour - timeIn.hour) / 100;
+}
+
+function wagesEarnedOnDate(date) {
+    return hoursWorkedOnDate.call(this, date) * this.payPerHour;
+}
+
+function findEmployeeByFirstName(srcArray, firstName) {
+    const employee = srcArray.find(record => record.firstName === firstName)
+    return employee;
+}
+  
+function calculatePayroll(records) {
+    const payroll = records.reduce((acc, cur) => {
+      return acc + allWagesFor.call(cur)
+    }, 0);
+    return payroll;
+}
